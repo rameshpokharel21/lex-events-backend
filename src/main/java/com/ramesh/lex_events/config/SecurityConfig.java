ce5369c0219eaf8d9d0385347d42b71e1fa8a75c
rameshpokharel21 <pokharelramesh@yahoo.com>
2025-09-18 19:04:58 -0400

    allowed origins in config

--- src/main/java/com/ramesh/lex_events/config/SecurityConfig.java
+++ src/main/java/com/ramesh/lex_events/config/SecurityConfig.java
@@ -34,6 +34,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
@@ -49,6 +50,8 @@ public class SecurityConfig {
     private final JwtAccessDeniedHandler accessDeniedHandler;
     private final JwtUtils jwtUtils;
 
+    @Value("${app.cors.allowed-origins}")
+    private String allowedOrigins;
 
     public SecurityConfig(UserDetailsService userDetailsService, AuthEntryPointJwt unauthorizedHandler, JwtAccessDeniedHandler accessDeniedHandler, JwtUtils jwtUtils) {
         logger.info("Starting constructor in SecurityConfig");
@@ -175,7 +178,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        List<String> origins = Arrays.asList(allowedOrigins.split(","));
+        config.setAllowedOrigins(origins);
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
