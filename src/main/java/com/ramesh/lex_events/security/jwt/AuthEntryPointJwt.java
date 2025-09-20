d0ed464f5c256b0695d393cd65bb7387392ddc5b
rameshpokharel21 <pokharelramesh@yahoo.com>
2025-09-19 20:28:55 -0400

    removing log on AuthEntryPoint

--- src/main/java/com/ramesh/lex_events/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/ramesh/lex_events/security/jwt/AuthEntryPointJwt.java
@@ -19,7 +19,7 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
 
-        logger.error("Unauthorized error: {}", authException.getMessage());
+        //logger.error("Unauthorized error: {}", authException.getMessage());
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         String message = "Unauthorized access: " + authException.getMessage();
